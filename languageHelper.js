2292dd696ec7907c5334042ec760f5a75978f973
Jona <j.geishauser@web.de>
2023-06-21 14:06:14 +0200

    fix language map, remove de com

--- languageHelper.js
+++ languageHelper.js
@@ -16,20 +16,19 @@ const SEPARATOR_MAP = {
     verticalSeparator: '.',
     horizontalSeparator: ';',
     argumentSeparator: ';',
-    reformatNumber: reformatNumberForSemiColonArgumentSeparator,
+    reformatNumberForJsParsing: reformatNumberForSemiColonArgumentSeparator,
     isScientificNotation: isScientificNotationForSemiColonArgumentSeparator
   },
   ".": {
     verticalSeparator: ';',
     horizontalSeparator: ',',
     argumentSeparator: ',',
-    reformatNumber: reformatNumberForCommaArgumentSeparator,
+    reformatNumberForJsParsing: reformatNumberForCommaArgumentSeparator,
     isScientificNotation: isScientificNotationForCommaArgumentSeparator
   }
 }
 
 function getLanguageObjectFromLanguageAndSeparator(displayLanguage, decimalSeparator) {
-  console.log("in getLang",displayLanguage,decimalSeparator)
   if (!SEPARATOR_MAP[decimalSeparator]) throw new Error("Invalid decimal separator");
   const isDELanguage = displayLanguage.match(/de\-/);
   const trueValue = isDELanguage ?  'WAHR' : "TRUE";
